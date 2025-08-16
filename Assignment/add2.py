75dbeb4900704248e56ed14998601e4ef446750e
Despina N.A. <despinannebs22@gmail.com>
2025-08-16 18:23:32 +0100

    Edited my calculator to perform an assigned task

--- Assignment/add2.py
+++ Assignment/add2.py
@@ -12,14 +12,38 @@ num1 = int(input("Number 1: "))
 print("What is your second number?")
 num2 = int(input("Number 2: "))
 
-sum = num1 + num2 
-print(f"This is the sum of the two numbers; {sum}") #addition
+print("What operation do you want to perform on the numbers?")
+operation = input("Operation: ").lower()
 
-sum = num1 - num2
-print(f"This is the difference of the two numbers; {sum}") #subtraction
+if operation == "sum":
+    sum = num1 + num2 
+    print(f"This is the sum of the two numbers; {sum}") #addition
 
-sum = num1 * num2
-print(f"This is the product of the two numbers; {sum}") #multiplication
+elif operation == "difference":
+    difference = num1 - num2
+    print(f"This is the difference of the two numbers; {difference}") #subtraction 
 
-sum = num1 / num2
-print(f"This is the quotient of the two numbers; {sum}") #division
+elif operation == "product":
+    product = num1 * num2
+    print(f"This is the product of the two numbers; {product}") #multiplication 
+
+elif operation == "quotient":
+    quotient = num1 / num2
+    print(f"This is the quotient of the two numbers; {quotient}") #division
+
+else:
+    print("Operation unavailable. Input operation again.")
+    operation = input("Operation: ").lower()
+
+
+# sum = num1 + num2 
+# print(f"This is the sum of the two numbers; {sum}") #addition
+
+# difference = num1 - num2
+# print(f"This is the difference of the two numbers; {difference}") #subtraction
+
+# product = num1 * num2
+# print(f"This is the product of the two numbers; {product}") #multiplication
+
+# quotient = num1 / num2
+# print(f"This is the quotient of the two numbers; {quotient}") #division
